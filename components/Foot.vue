44c2828069ae8942f31936049bff80a9c4549f60
probablyjassin <aouanijassin@gmail.com>
2023-11-22 23:42:35 +0100

    remove notice

--- components/Foot.vue
+++ components/Foot.vue
@@ -2,6 +2,5 @@
     <footer class="bg-dark text-white text-center p-2">
 			Jässin Aouani
 			<a href="https://github.com/probablyjassin" class="link-light"><i class="fab fa-github"></i></a>
-			<a href="https://script.aouani.de"><p class="text-blue-300">Diese Seite wird von meinem Raspberry Pi bereitgestellt</p></a>
 		</footer>
 </template>
