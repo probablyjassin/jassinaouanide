f45b0fb083a2bd37917d2d64e861421736d46aea
probablyjassin <aouanijassin@gmail.com>
2023-11-22 23:51:03 +0100

    no ssr I guess

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: true,
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss',],
 })
