4e2fd4cd189a403579ad77c310a7c60df40362d8
probablyjassin <aouanijassin@gmail.com>
2023-11-23 12:22:41 +0100

    update footer

--- components/Foot.vue
+++ components/Foot.vue
@@ -2,5 +2,6 @@
     <footer class="bg-dark text-white text-center p-2">
 			Jässin Aouani
 			<a href="https://github.com/probablyjassin" class="link-light"><i class="fab fa-github"></i></a>
+			<a href="https://script.aouani.de" class="text-blue-300">Diese Seite wird von meinem Raspberry Pi bereitgestellt</a>
 		</footer>
 </template>
