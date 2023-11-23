0474ce5f85a2d009d8eb73953fc8d0bb9f8e5e9b
probablyjassin <aouanijassin@gmail.com>
2023-11-23 12:38:27 +0100

    flex-col the footer

--- components/Foot.vue
+++ components/Foot.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-dark text-white text-center p-2">
+    <footer class="bg-dark text-white text-center p-2 flex flex-col">
 			Jässin Aouani
 			<a href="https://github.com/probablyjassin" class="link-light"><i class="fab fa-github"></i></a>
 			<a href="https://script.aouani.de" class="text-blue-300">Diese Seite wird von meinem Raspberry Pi bereitgestellt</a>
